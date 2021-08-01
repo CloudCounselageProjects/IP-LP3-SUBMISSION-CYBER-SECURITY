2f6fa0210cf762e4ea1484d226fd2380142af1b7
MeghaKM8 <88278539+MeghaKM8@users.noreply.github.com>
2021-08-02 00:04:28 +0530

    Add files via upload

---
+++ IP_LP3_Cyber_Security_Megha_K.M_4890/cryptography/DecryptFile.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+
+key = "1p_7JlbvMIKfmNqMkhaNkuLdk31QzkXtO6mB7MusmLQ="
+
+system_information_e = "e_systeminfo.txt"
+clipboard_information_e = "e_clipinfo.txt"
+keys_information_e = "e_key_log.txt"
+
+
+encrypted_files = [system_information_e, clipboard_information_e, keys_information_e]
+count = 0
+
+for decrypting_file in encrypted_files:
+
+    with open(encrypted_files[count], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+
+    with open(encrypted_files[count], 'wb') as f:
+        f.write(decrypted)
+
+    count += 1
